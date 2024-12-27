f9ed644e79b77f2be91d81d299bbb6d2471ab716
ZackFair <sockwz@gmail.com>
2024-12-27 09:05:56 +0800

    修改DB路径

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,9 +1,11 @@
 import sqlite3
 from typing import Generator
 
+from app.core.config import settings
+
 
 def get_db() -> Generator[sqlite3.Connection, None, None]:
-    conn = sqlite3.connect("speakers.db", check_same_thread=False)  # 允许多线程共享
+    conn = sqlite3.connect(settings.SPEAKERS_DB, check_same_thread=False)  # 允许多线程共享
     try:
         yield conn
     finally:
